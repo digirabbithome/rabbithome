3f0ad9cc0df8997b6dab65df88506720f3cce19d
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 02:53:49 +0800

    Add files via upload

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,15 +1,1 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyANuDJyJuQbxnXq-FTyaTAI9mSc6zpmuWs",
-  authDomain: "rabbithome-auth.firebaseapp.com",
-  projectId: "rabbithome-auth",
-  storageBucket: "rabbithome-auth.appspot.com",
-  messagingSenderId: "50928677930",
-  appId: "1:50928677930:web:e8eff13c8028b888537f53"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-export { auth };
+// placeholder script
--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,1 @@
-import { auth } from './firebase.js';
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
-
-window.login = function () {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      localStorage.setItem("uid", userCredential.user.uid);
-      location.href = "main.html";
-    })
-    .catch((error) => {
-      document.getElementById("error-message").innerText = "登入失敗：" + error.message;
-    });
-}
+// placeholder script
