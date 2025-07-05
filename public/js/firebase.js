a1f25ac014c706c974547a33773dc311461c790e
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 00:50:09 +0800

    Add files via upload

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyD-sample1234567890abcdeFgHIJK",
-  authDomain: "rabbithome-demo.firebaseapp.com",
-  projectId: "rabbithome-demo",
-  storageBucket: "rabbithome-demo.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdefg1234567"
+  apiKey: "AIzaSyANuDJyJuQbxnXq-FTyaTAI9mSc6zpmuWs",
+  authDomain: "rabbithome-auth.firebaseapp.com",
+  projectId: "rabbithome-auth",
+  storageBucket: "rabbithome-auth.appspot.com",
+  messagingSenderId: "50928677930",
+  appId: "1:50928677930:web:e8eff13c8028b888537f53"
 };
 firebase.initializeApp(firebaseConfig);
