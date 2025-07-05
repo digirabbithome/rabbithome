dd4a994e76cb8ea9bef79f41fc2731a900d7a4e0
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 01:04:03 +0800

    Delete js/firebase.js

--- js/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyANuDJyJuQbxnXq-FTyaTAI9mSc6zpmuWs",
-  authDomain: "rabbithome-auth.firebaseapp.com",
-  projectId: "rabbithome-auth",
-  storageBucket: "rabbithome-auth.appspot.com",
-  messagingSenderId: "50928677930",
-  appId: "1:50928677930:web:e8eff13c8028b888537f53"
-};
-firebase.initializeApp(firebaseConfig);
