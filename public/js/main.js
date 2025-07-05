7eb4b12a5067854c68310a2f059430daf2296314
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 01:58:47 +0800

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -27,5 +27,15 @@ onAuthStateChanged(auth, async (user) => {
     } else {
       document.getElementById("nickname").innerText = "Hello，使用者！";
     }
+  }
+});
+
+document.getElementById("logout-button").addEventListener("click", async () => {
+  try {
+    await signOut(auth);
+    window.location.href = "login.html";
+  } catch (error) {
+    console.error("登出失敗：", error);
+    alert("登出失敗！");
   }
 });
