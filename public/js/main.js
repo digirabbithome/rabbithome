8bd67d87295ddc18eddd4c9c5ab91f9abb95d3e8
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 02:34:06 +0800

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,9 @@
-import { auth } from './firebase.js';
-import { signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+document.getElementById("btn-add-user").addEventListener("click", loadAddUser);
 
-const nicknameEl = document.getElementById("nickname");
-const workspace = document.getElementById("workspace");
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    const uid = user.uid;
-    fetch(`https://firestore.googleapis.com/v1/projects/rabbithome-auth/databases/(default)/documents/users/${uid}`)
-      .then(res => res.json())
-      .then(data => {
-        const nickname = data.fields?.nickname?.stringValue || "未知使用者";
-        nicknameEl.innerText = `Hello，${nickname}！`;
-      });
-  } else {
-    location.href = "login.html";
-  }
-});
-
-document.getElementById("logout").addEventListener("click", () => {
-  signOut(auth).then(() => {
-    location.href = "login.html";
-  });
-});
+function loadAddUser() {
+  fetch("public/pages/add-user.html")
+    .then((res) => res.text())
+    .then((html) => {
+      document.getElementById("content-area").innerHTML = html;
+    });
+}
