93ade2cca88ed59aeb9a082f7f3d3e955b0afed5
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 01:54:17 +0800

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-  const userEmail = sessionStorage.getItem("userEmail") || "花花";
-  document.getElementById("nickname").textContent = `Hello，${userEmail}！`;
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+const auth = getAuth();
+const db = getFirestore();
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const nickname = docSnap.data().nickname || "使用者";
+      document.getElementById("nickname").innerText = `Hello，${nickname}！`;
+    } else {
+      document.getElementById("nickname").innerText = "Hello，使用者！";
+    }
+  }
 });
