801b1ec30379d97905bea319b5fcc1c85a4deafd
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 02:36:48 +0800

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,1 @@
-document.getElementById("btn-add-user").addEventListener("click", loadAddUser);
-
-function loadAddUser() {
-  fetch("public/pages/add-user.html")
-    .then((res) => res.text())
-    .then((html) => {
-      document.getElementById("content-area").innerHTML = html;
-    });
-}
+document.addEventListener('DOMContentLoaded', () => { console.log('Main loaded'); });
