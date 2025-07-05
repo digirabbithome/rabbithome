0dc7f6b8678b6eac843ade503b932ff9de16c330
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 03:17:30 +0800

    Add files via upload

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,21 @@
-import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
-window.login = async function login() {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { firebaseConfig } from './firebase-config.js';
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.login = function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const errorMessage = document.getElementById("error-message");
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-    sessionStorage.setItem("userEmail", user.email);
-    window.location.href = "main.html";
-  } catch (error) {
-    errorMessage.textContent = "登入失敗：" + error.message;
-  }
-};
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      window.location.href = "main.html";
+    })
+    .catch((error) => {
+      errorMessage.textContent = "登入失敗：" + error.message;
+    });
+}
