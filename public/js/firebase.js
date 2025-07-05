9854de1d007884268ea5bd7c633ac89b23cc58ac
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 00:20:36 +0800

    Add files via upload

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,2 +1,11 @@
-// 請填入你的 Firebase 設定
-// firebase.initializeApp({...});
+// 🔐 請填入你的 Firebase 設定
+// https://console.firebase.google.com/
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+firebase.initializeApp(firebaseConfig);
