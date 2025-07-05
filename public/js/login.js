7b4c79f3a14544642e5345fed8659fd1510d8a4d
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-05 13:48:43 +0800

    Add files via upload

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,20 @@
-
 import { auth } from './firebase-init.js';
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("loginBtn").addEventListener("click", async () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+  const loginBtn = document.getElementById("loginBtn");
+  if (loginBtn) {
+    loginBtn.addEventListener("click", async () => {
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
 
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      alert("登入成功！");
-      window.location.href = "/main.html";
-    } catch (error) {
-      alert("登入失敗：" + error.message);
-    }
-  });
+      try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        alert("登入成功！");
+        window.location.href = "/main.html";
+      } catch (error) {
+        alert("登入失敗：" + error.message);
+      }
+    });
+  }
 });
