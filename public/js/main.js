a7c1eb86f56672cee392ae303b03848a3fb985ba
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 03:00:07 +0800

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,33 @@
-// placeholder script
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANuDJyJuQbxnXq-FTyaTAI9mSc6zpmuWs",
+  authDomain: "rabbithome-auth.firebaseapp.com",
+  projectId: "rabbithome-auth",
+  storageBucket: "rabbithome-auth.appspot.com",
+  messagingSenderId: "50928677930",
+  appId: "1:50928677930:web:e8eff13c8028b888537f53"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+    const nickname = docSnap.exists() ? docSnap.data().nickname : "使用者";
+    document.getElementById("nickname").innerText = `Hello，${nickname}！`;
+  } else {
+    window.location.href = "login.html";
+  }
+});
+
+document.getElementById("btn-logout").addEventListener("click", () => {
+  signOut(auth).then(() => {
+    window.location.href = "login.html";
+  });
+});
