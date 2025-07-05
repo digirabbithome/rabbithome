b407c45d9b83dff319d1e0fa9ccac4f29c29a6e9
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 01:13:08 +0800

    Add files via upload

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,4 +1,5 @@
-const firebaseConfig = {
+
+export const firebaseConfig = {
   apiKey: "AIzaSyANuDJyJuQbxnXq-FTyaTAI9mSc6zpmuWs",
   authDomain: "rabbithome-auth.firebaseapp.com",
   projectId: "rabbithome-auth",
@@ -6,4 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "50928677930",
   appId: "1:50928677930:web:e8eff13c8028b888537f53"
 };
-firebase.initializeApp(firebaseConfig);
--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,20 @@
-function login() {
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { firebaseConfig } from './firebase.js';
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.login = async function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(() => {
-      window.location.href = "main.html";
-    })
-    .catch((error) => {
-      document.getElementById("error-message").innerText = "登入失敗：" + error.message;
-    });
-}
+  const errorMsg = document.getElementById("error-message");
+
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "main.html";
+  } catch (error) {
+    errorMsg.textContent = "登入失敗：" + error.message;
+  }
+};
