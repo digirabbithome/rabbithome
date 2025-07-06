8381a411e3aa954e0441702c310ea0c2397b9cbf
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 16:53:49 +0800

    Add files via upload

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,4 +1,8 @@
-export const firebaseConfig = {
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+const firebaseConfig = {
   apiKey: "AIzaSyANuDJyJuQbxnXq-FTyaTAI9mSc6zpmuWs",
   authDomain: "rabbithome-auth.firebaseapp.com",
   projectId: "rabbithome-auth",
@@ -6,3 +10,8 @@ export const firebaseConfig = {
   messagingSenderId: "50928677930",
   appId: "1:50928677930:web:e8eff13c8028b888537f53"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
