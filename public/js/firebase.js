2889896622a3b9a6ab0606824bcc0bb9b1401f4e
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 15:12:09 +0800

    Add files via upload

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,20 +1,16 @@
-
-// 請使用你自己的 Firebase 設定值替換以下內容
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "你的-api-key",
-  authDomain: "你的-auth-domain",
-  projectId: "你的-project-id",
-  storageBucket: "你的-storage-bucket",
-  messagingSenderId: "你的-messaging-sender-id",
-  appId: "你的-app-id"
+  apiKey: "AIzaSyAKl-xxxxxxx",  // ✅ 保護設定
+  authDomain: "rabbithome.firebaseapp.com",
+  projectId: "rabbithome",
+  storageBucket: "rabbithome.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:xxxxxxxx"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
