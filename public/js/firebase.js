5fbbde3135f6765935cca98f7a782138f4006a2c
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 13:40:59 +0800

    Add files via upload

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,3 +1,4 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const nicknameEl = document.getElementById("nickname");
-  if (nicknameEl) {
-    const userData = JSON.parse(localStorage.getItem("user"));
-    if (userData && userData.nickname) {
-      nicknameEl.textContent = `Hello，${userData.nickname}！`;
-    } else {
-      nicknameEl.textContent = "Hello！";
-    }
+
+import { auth } from './firebase.js';
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+const nicknameSpan = document.getElementById("nickname");
+const contentDiv = document.getElementById("content");
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    fetch(`https://firestore.googleapis.com/v1/projects/rabbithome-auth/databases/(default)/documents/users/${user.uid}`)
+      .then(response => response.json())
+      .then(data => {
+        const nickname = data.fields?.nickname?.stringValue || "使用者";
+        nicknameSpan.textContent = nickname;
+      });
+  } else {
+    window.location.href = "login.html";
   }
+});
+
+document.getElementById("logout-btn").addEventListener("click", () => {
+  signOut(auth).then(() => {
+    window.location.href = "login.html";
+  });
+});
+
+document.getElementById("btn-daily").addEventListener("click", () => {
+  contentDiv.innerHTML = "<h2>這裡是每日工作區域</h2>";
+});
+
+document.getElementById("btn-adduser").addEventListener("click", () => {
+  contentDiv.innerHTML = `
+    <h2>新增帳號</h2>
+    <input type="text" placeholder="Email" /><br/>
+    <input type="password" placeholder="密碼" /><br/>
+    <input type="text" placeholder="姓名" /><br/>
+    <input type="text" placeholder="綽號" /><br/>
+    <input type="date" placeholder="生日" /><br/>
+    <label><input type="checkbox" value="外場" /> 外場</label>
+    <label><input type="checkbox" value="內場" /> 內場</label>
+    <label><input type="checkbox" value="美編" /> 美編</label>
+    <label><input type="checkbox" value="出貨" /> 出貨</label><br/>
+    <button>送出</button>
+  `;
 });
