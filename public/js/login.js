9edd4ae81766a9771753dadc00d778031862b784
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 16:19:16 +0800

    Add files via upload

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,9 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { firebaseConfig } from "./firebase.js";
 
-import { auth } from './firebase.js';
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
-import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
-
-const db = getFirestore();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 window.login = function () {
   const email = document.getElementById("email").value;
@@ -11,15 +11,9 @@ window.login = function () {
   const errorMessage = document.getElementById("error-message");
 
   signInWithEmailAndPassword(auth, email, password)
-    .then(async (userCredential) => {
+    .then((userCredential) => {
       const user = userCredential.user;
-      const userDoc = await getDoc(doc(db, "users", user.uid));
-      const userData = userDoc.exists() ? userDoc.data() : {};
-      localStorage.setItem("user", JSON.stringify({
-        uid: user.uid,
-        email: user.email,
-        nickname: userData.nickname || "使用者"
-      }));
+      localStorage.setItem("user", JSON.stringify({ email: user.email }));
       window.location.href = "main.html";
     })
     .catch((error) => {
