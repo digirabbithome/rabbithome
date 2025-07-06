4d6090acbc2b3d6d74d0bf424df7eccd0159efa3
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 18:43:10 +0800

    Add files via upload

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyANuDJyJuQbxnXq-FTyaTAI9mSc6zpmuWs",
-  authDomain: "rabbithome-auth.firebaseapp.com",
-  projectId: "rabbithome-auth",
-  storageBucket: "rabbithome-auth.appspot.com",
-  messagingSenderId: "50928677930",
-  appId: "1:50928677930:web:e8eff13c8028b888537f53"
-};
+import { firebaseConfig } from "./firebase.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
