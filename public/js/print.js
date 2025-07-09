d5eb2c5a3a14b5e20d971da6e1f08b4d302df54f
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-09 18:54:02 +0800

    Update print.js

--- public/js/print.js
+++ public/js/print.js
@@ -5,21 +5,15 @@ window.addEventListener('load', () => {
   const senderMap = {
     '數位小兔': {
       name: '數位小兔 Digital Rabbit　台北市信義區大道路74巷1號',
-      address: '',
       phone: '02-27592006 / 02-27592013　LINE：@digirabbit',
-      line: ''
     },
     '聚焦數位': {
-      name: '聚焦數位 Focus Digital',
-      address: '台北市中山區範例路10號',
-      phone: '02-23456789',
-      line: 'LINE：@focuscam'
+      name: '聚焦數位 Focus Digital　台北市中山區範例路10號',
+      phone: '02-23456789　LINE：@focuscam',
     },
     '免睡攝影': {
-      name: '免睡攝影 No Sleep Studio',
-      address: '新北市板橋區攝影街88號',
-      phone: '02-87654321',
-      line: 'LINE：@nosleep'
+      name: '免睡攝影 No Sleep Studio　新北市板橋區攝影街88號',
+      phone: '02-87654321　LINE：@nosleep',
     }
   };
 
