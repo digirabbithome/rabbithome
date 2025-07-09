9db17f0cc9c88a70f09fba48eddb44a37291cabf
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-09 23:26:42 +0800

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -12,3 +12,8 @@ window.logout = function () {
   localStorage.removeItem("nickname");
   window.location.href = "login.html";
 };
+
+window.onload = function () {
+  const nickname = localStorage.getItem("nickname") || "使用者";
+  document.getElementById("nickname-display").textContent = `🙋‍♂️ ${nickname}`;
+};
