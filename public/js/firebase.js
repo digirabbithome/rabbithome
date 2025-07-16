ab4c1b2938d36b92692ec086d2e2462e9817e58e
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-17 01:48:55 +0800

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -7,8 +7,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyANuDJyJuQbxnXq-FTyaTAI9mSc6zpmuWs",
   authDomain: "rabbithome-auth.firebaseapp.com",
   projectId: "rabbithome-auth",
-//  storageBucket: "rabbithome-auth.firebasestorage.app",
-  storageBucket: "rabbithome-auth.appspot.com",
+  storageBucket: "rabbithome-auth.firebasestorage.app",
+//  storageBucket: "rabbithome-auth.appspot.com",
   messagingSenderId: "50928677930",
   appId: "1:50928677930:web:e8eff13c8028b888537f53"
 };
