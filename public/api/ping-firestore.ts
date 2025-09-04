d4bf264380f584202fe3890d29c1e7f69126cb10
digirabbithome <swimming8250@yahoo.com.tw>
2025-09-04 23:11:10 +0800

    Add files via upload

--- public/api/ping-firestore.ts
+++ public/api/ping-firestore.ts
@@ -6,7 +6,7 @@ if (!admin.apps.length){
     credential: admin.credential.cert({
       projectId: process.env.FB_PROJECT_ID,
       clientEmail: process.env.FB_CLIENT_EMAIL,
-      privateKey: (process.env.FB_PRIVATE_KEY || '').replace(/\n/g, '\n'),
+      privateKey: (process.env.FB_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
     } as any),
   })
 }
