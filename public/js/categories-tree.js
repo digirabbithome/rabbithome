fd8c099bd7f234cfbc7cb6e8d1c8fe2e1b15b042
digirabbithome <swimming8250@yahoo.com.tw>
2025-09-18 00:27:15 +0800

    Add files via upload

--- public/js/categories-tree.js
+++ public/js/categories-tree.js
@@ -195,6 +195,9 @@ function renderNode(node, depth){
       applyDropSortFn: (typeof applyDropSort==='function' ? applyDropSort : (typeof window!=='undefined' ? window.applyDropSort : undefined)),
       onPick: undefined,
       onCancel: undefined
+    })),
+      onPick: undefined,
+      onCancel: undefined
     })
   })
 
@@ -257,7 +260,16 @@ function renderNode(node, depth){
 }
 
 
-function showDropChoiceMenu({li, node, clientX, clientY, onPick, onCancel}, applyDropSortFn: (typeof applyDropSort==='function' ? applyDropSort : (window && window.applyDropSort)), onPick: undefined, onCancel: undefined }{
+function showDropChoiceMenu({
+      li,
+      node,
+      draggedId,
+      clientX: cx,
+      clientY: cy,
+      applyDropSortFn: (typeof applyDropSort==='function' ? applyDropSort : (typeof window!=='undefined' ? window.applyDropSort : undefined)),
+      onPick: undefined,
+      onCancel: undefined
+    })), onPick: undefined, onCancel: undefined }{
   document.querySelectorAll('.tree-drop-menu').forEach(m=>m.remove())
   const menu = document.createElement('div')
   menu.className = 'tree-drop-menu'
