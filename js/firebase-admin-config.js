166ee090e33c00cc43c33d36c112c8742e129e4e
digirabbithome <swimming8250@yahoo.com.tw>
2025-10-08 20:50:09 +0800

    Update firebase-admin-config.js

--- js/firebase-admin-config.js
+++ js/firebase-admin-config.js
@@ -1,13 +1,40 @@
 export const serviceAccount = {
-  "type": "service_account",
-  "project_id": "rabbithome-auth",
-  "private_key_id": "51233dc7225f7631c2aab1248cf4c483361e3ceb",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDM9xIsrboxdph3\nN6rHVKtZJ50ta4FlZ0PN+MkaJdLrXG/UIGLolbSo8Uv7v+g/25SzYzCwUe8EQNEP\n18P07MN2brF4YAnh1sPn8neElzrQShMKJ0KEHQ02sg0SfiArP3F7qLBLbcXsX1R7\nNCtPQQbrx3oX0Azcq89/7DufbZlQ02ZcQTLhbMG471HVOAqzl9db3hLWXsYL7GGu\n/87SVhAvtQryyHkWV1d3vxvXJ6S6QQzoIuG8LHLSpHd0TTZ8qGSPGKQxLc1nZjOG\n5Cms1Fx4/jsd8fIgVYEMa+E1U8niYiUChowlvL54quRCowMnutlm4Du+yERkBLgk\n3euRdXVxAgMBAAECggEAJZ+cyHLDYN0CM7AWOyzBn0X0Rigbwn9oJODsGU6kHuwC\n4w9m2IbSeMiCSB38BezKqPsmLtd7Zm53cc8Le2QSpSTAkvYeGkSQBX2ujror45wV\nTqHSReoHSOj6GVDpmsa0kBxw8Aegczk0isESyiOHn7QoJ0LbeY5YMoUslKFLqVgw\npv1ZFVDwj1+csSWtgcjG+ARH2f3pSvCHrMfz27zP0I9vuZ59gKX4F1muGHWyXEhl\n6mAgU16i9JeuL/ydl/Hn36U6RUZRiQs3M+j6noAvA7AOYNx4lJBNvPu9Xwhd7Vqs\nQCeRxRsY8APCR6YWeZSnJBTw0sSzSE0NO7QqnkUGOQKBgQDs9lz3AVSfoRcg8n1n\nhdxU2+pNrEpJGuaOenQVhKIlOoo1KnE8bjLu+HxEFUhcSjk8KznLtTVjNWlDNeC+\nCRF6k0xFYgNlhixSmp/cSAl38uRkJQiNKII/Gi59T1v4BTW2PERymSltkvQ5HiRG\npcSVTNf/wtsOaEFak/dB1VPqjwKBgQDdbp3Li3EAa9rUC6oGZeftOVxX8u1OJk/d\n5w0ttmMFMCv6Z43cRrDBlxmHUMBd43AVdNAAzOR0in3AccbgoIs2urNC9tG8SlV+\ntmGzQGLtomxc2O5M/A/8E9S6G+XA+TVAR/FzH8Xgbe8jxV1zm+ZVR8hf6UC2iWWH\nr0JfoNCf/wKBgQDduczVR4Zyc56bsqsLEiNPuBNuqJc9/pDbotM6S0gZg8RM6TDZ\ntdes6hAE+NEjokQZ++xxo1aYRSMAw8pOp1eWlXuZtJOvDLQuewa4bSOG/F0fI9Pz\nmYL+HrZ/hKFu2xkeMxhtDsnIAOjLBVw2Sug+N5Apfp/QxFbatXd4P2/tGwKBgQDL\neIK878QFjBYHtTQmLuwAGy5UjqAqqJ/JGzAL6kVxFgPDU/aSC88OBMdrhPNPEtgC\nj1U29w+SW1QUSzhhXoSt5L8mcGDncTLAfdym9yowjIighKHs0c3qZ1qZLVtgiF67\nU6B1liOhB8OwDgLCYeY0RQLFxzM0yGooVQ5biw1JhwKBgG/gZ0qZkhPCCdJscCq+\nCtQff6NmlgcFZybpZ4W/bbLhkmk6J4ZNAoEoScLlpuDFm53H5Kd7zQb1y7dqdSep\nOmdk7czNKAWbxX4ck5SrseWj1fjjkynMpZ4M7QvdOCwZRikXj+Wit5Y6DscAJ/Fn\niUGhScGtsgsgyGaIadNTHCTq\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-fbsvc@rabbithome-auth.iam.gserviceaccount.com",
-  "client_id": "110951543289526757002",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40rabbithome-auth.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
+  type: "service_account",
+  project_id: "rabbithome-auth",
+  private_key_id: "51233dc7225f7631c2aab1248cf4c483361e3ceb",
+  private_key: `-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDM9xIsrboxdph3
+N6rHVKtZJ50ta4FlZ0PN+MkaJdLrXG/UIGLolbSo8Uv7v+g/25SzYzCwUe8EQNEP
+18P07MN2brF4YAnh1sPn8neElzrQShMKJ0KEHQ02sg0SfiArP3F7qLBLbcXsX1R7
+NCtPQQbrx3oX0Azcq89/7DufbZlQ02ZcQTLhbMG471HVOAqzl9db3hLWXsYL7GGu
+/87SVhAvtQryyHkWV1d3vxvXJ6S6QQzoIuG8LHLSpHd0TTZ8qGSPGKQxLc1nZjOG
+5Cms1Fx4/jsd8fIgVYEMa+E1U8niYiUChowlvL54quRCowMnutlm4Du+yERkBLgk
+3euRdXVxAgMBAAECggEAJZ+cyHLDYN0CM7AWOyzBn0X0Rigbwn9oJODsGU6kHuwC
+4w9m2IbSeMiCSB38BezKqPsmLtd7Zm53cc8Le2QSpSTAkvYeGkSQBX2ujror45wV
+TqHSReoHSOj6GVDpmsa0kBxw8Aegczk0isESyiOHn7QoJ0LbeY5YMoUslKFLqVgw
+pv1ZFVDwj1+csSWtgcjG+ARH2f3pSvCHrMfz27zP0I9vuZ59gKX4F1muGHWyXEhl
+6mAgU16i9JeuL/ydl/Hn36U6RUZRiQs3M+j6noAvA7AOYNx4lJBNvPu9Xwhd7Vqs
+QCeRxRsY8APCR6YWeZSnJBTw0sSzSE0NO7QqnkUGOQKBgQDs9lz3AVSfoRcg8n1n
+hdxU2+pNrEpJGuaOenQVhKIlOoo1KnE8bjLu+HxEFUhcSjk8KznLtTVjNWlDNeC+
+CRF6k0xFYgNlhixSmp/cSAl38uRkJQiNKII/Gi59T1v4BTW2PERymSltkvQ5HiRG
+pcSVTNf/wtsOaEFak/dB1VPqjwKBgQDdbp3Li3EAa9rUC6oGZeftOVxX8u1OJk/d
+5w0ttmMFMCv6Z43cRrDBlxmHUMBd43AVdNAAzOR0in3AccbgoIs2urNC9tG8SlV+
+tmGzQGLtomxc2O5M/A/8E9S6G+XA+TVAR/FzH8Xgbe8jxV1zm+ZVR8hf6UC2iWWH
+r0JfoNCf/wKBgQDduczVR4Zyc56bsqsLEiNPuBNuqJc9/pDbotM6S0gZg8RM6TDZ
+tdes6hAE+NEjokQZ++xxo1aYRSMAw8pOp1eWlXuZtJOvDLQuewa4bSOG/F0fI9Pz
+mYL+HrZ/hKFu2xkeMxhtDsnIAOjLBVw2Sug+N5Apfp/QxFbatXd4P2/tGwKBgQDL
+eIK878QFjBYHtTQmLuwAGy5UjqAqqJ/JGzAL6kVxFgPDU/aSC88OBMdrhPNPEtgC
+j1U29w+SW1QUSzhhXoSt5L8mcGDncTLAfdym9yowjIighKHs0c3qZ1qZLVtgiF67
+U6B1liOhB8OwDgLCYeY0RQLFxzM0yGooVQ5biw1JhwKBgG/gZ0qZkhPCCdJscCq+
+CtQff6NmlgcFZybpZ4W/bbLhkmk6J4ZNAoEoScLlpuDFm53H5Kd7zQb1y7dqdSep
+Omdk7czNKAWbxX4ck5SrseWj1fjjkynMpZ4M7QvdOCwZRikXj+Wit5Y6DscAJ/Fn
+iUGhScGtsgsgyGaIadNTHCTq
+-----END PRIVATE KEY-----`,
+  client_email: "firebase-adminsdk-fbsvc@rabbithome-auth.iam.gserviceaccount.com",
+  client_id: "110951543289526757002",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40rabbithome-auth.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
 };
