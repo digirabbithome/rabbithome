1a45620f0e20e4b3c3c254fc9452c349abe91647
digirabbithome <swimming8250@yahoo.com.tw>
2025-12-03 00:18:42 +0800

    Add files via upload

--- public/js/clean-db.js
+++ public/js/clean-db.js
@@ -1,1 +1,20 @@
-// JS content trimmed for brevity, same as provided
+import { db } from '/js/firebase.js'
+import {collection,getDocs,deleteDoc,doc} 
+from 'https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js'
+
+const $=s=>document.querySelector(s)
+const log=a=>{$('#log-area').innerHTML+=a+"<br>"}
+
+async function calc(){
+  log("calc start")
+  const snap=await getDocs(collection(db,'bulletins'))
+  $('#result-bulletins').textContent = snap.size
+}
+async function clean(){
+  log("clean start")
+}
+window.onload=()=>{
+ $('#btn-calc-bulletins').onclick=calc
+ $('#btn-clean-bulletins').onclick=clean
+ $('#clear-log').onclick=()=>$('#log-area').innerHTML=""
+}
